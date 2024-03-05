de65fe53fed0e02f36d0888c192636ba721ff533
Mahary <maharyfenitra@gmail.com>
2024-03-05 12:20:11 +0300

    Asset

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./about.module.css";
+
+const page = () => {
+    return (
+        <div>
+            About page
+        </div>
+    )
+}
+
+export default page; 
